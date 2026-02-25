5ce0abbe21abd097ff4b393bef0c2f9e333eb0c6
shamil <for.photos.only001@gmail.com>
2026-02-25 22:22:46 +0530

    running sum of 1d array[efficient solution]

--- Running_Sum_of_1d_Array/sloution.js
+++ Running_Sum_of_1d_Array/sloution.js
@@ -20,18 +20,46 @@
 // let arr = [1,2,3,4]
 
 // console.log(runningSum(arr))
+// var runningSum = function(nums) {
+//     let ar = nums
+//     let result = ar.map((e,i)=>{
+//         let s = 0
+//         for(let k = 0;k<=i;k++){
+//             s += ar[k]
+//         }
+//         return s
+//     })
+    
+//     return result
+    
+// };
+
+
+// let arr = [1,2,3,4]
+
+// console.log(runningSum(arr))
+// var runningSum = function(nums) {
+//     let ar = nums
+//     // let result = []
+//     let s = 0
+//     for(let i = 0;i<ar.length;i++){
+//         s += ar.shift()
+//         ar.push(s)
+//     }
+//     return ar
+// };
+
+
+// let arr = [1,2,3,4]
+
+// console.log(runningSum(arr))
 var runningSum = function(nums) {
-    let ar = nums
-    let result = ar.map((e,i)=>{
-        let s = 0
-        for(let k = 0;k<=i;k++){
-            s += ar[k]
-        }
-        return s
-    })
-    
-    return result
-    
+    let s = 0
+    for(let i = 0;i<nums.length;i++){
+        s += nums.shift()
+        nums.push(s)
+    }
+    return nums
 };
 
 
